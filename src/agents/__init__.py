cbbe2ec7fbf3afe53533306d3c7f9b1d71d5b230
Imbue <imbue2025@outlook.com>
2025-07-12 22:57:44 +0800

    Create __init__.py

---
+++ src/agents/__init__.py
@@ -0,0 +1,4 @@
+from .textgan_agents import GeneratorAgent, RewarderAgent, ReviewerAgent
+from .deep_research_agent import DeepResearchAgent
+
+__all__ = ['GeneratorAgent', 'RewarderAgent', 'ReviewerAgent', 'DeepResearchAgent'] 
